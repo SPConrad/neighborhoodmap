eb9d7dbc61e8579469e83549d7d1e93fc00505f5
Konrad9 <konrad9@gmail.com>
2016-09-16 18:09:12 -0400

    I'm really dumb
    set old = current and was not setting current to be a new object.
    
    reference/value

--- js/map.js
+++ js/map.js
@@ -59,10 +59,9 @@ function Map()
 
 		self.currentNearbyPlace = "";
 
-		self.oldNearbyPlace;
+		self.oldNearbyPlace = "";
 
-		self.currentBigMarker = "";
-		
+		self.currentBigMarker = "";		
 
 		//self.searchBox = new google.maps.places.SearchBox(document.getElementById("search-text"));
 		
@@ -96,30 +95,36 @@ function Map()
 	this.NearbyPlaceObject = function(index){
 		this.button = $('#nearby-places-' + index);
 		this.marker = self.nearbyMarkers[index];
-
-		return this;
+		this.index = index;
 	}
 
-	this.changeCurrentNearbyMarker = function(index){
-		console.log(self.oldNearbyPlace === self.currentNearbyPlace);
-		if (self.oldNearbyPlace != self.currentNearbyPlace){
-			self.oldNearbyPlace = self.currentNearbyPlace;
-
-			if (self.oldNearbyPlace != "") {
-				self.oldNearbyPlace.button.collapse("hide");
-				self.oldNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
-			}
-
-			self.currentNearbyPlace = self.NearbyPlaceObject(index);
-
-			self.currentNearbyPlace.button.collapse("show");
-			self.currentNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
-
-			console.log(self.currentNearbyPlace.button);
-			console.log(self.oldNearbyPlace.button);
+	this.changeOldMarker = function(index, data){
+		//console.log("======== change old marker =======")
+		//console.log("came from " + data);
+		if (self.oldNearbyPlace != "") {
+			//console.log("hide old button " + self.oldNearbyPlace.index);
+			self.oldNearbyPlace.button.collapse("hide");
+			//console.log("change old marker " + self.oldNearbyPlace.index);
+			self.oldNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');	
 		}
 
+		//console.log("change old from " + self.oldNearbyPlace.index + " to " + self.currentNearbyPlace + ", passed in index is " + index);
+		self.oldNearbyPlace = self.currentNearbyPlace;
+		
 	}
+	this.activateCurrentMarker = function(index, data){
+		//console.log("-----assign currentNearbyPlace----- " + index);
+		//console.log("came from: " + data);
+		self.currentNearbyPlace = self.NearbyPlaceObject(index);
+
+		//console.log("show button on index -------------- " + index);
+		self.currentNearbyPlace.button.collapse("show");
+		//console.log("change marker color --------------- " + index);
+		self.currentNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+	}
+
+
+
 
 	///when searching by a specific type
 	this.searchPlacesByType = function(currentPlace, placeType){
@@ -210,20 +215,10 @@ function Map()
 				google.maps.event.addListener(marker, 'click', function(){				
 					///show the name of the location
 					self.showInfo(this, results.name);	
-					self.changeCurrentNearbyMarker(results.index);
-					/*if (self.currentNearbyPlace != ""){
-						self.currentNearbyMarker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
-						self.toggleNearbyCollapse("hide");
-					}	
-					self.currentNearbyPlace = $("#nearby-places-" + results.index);
-					self.currentNearbyMarker = this;
-
-					///toggle the detail dropdown in the locations list 
-					self.toggleNearbyCollapse("show");*/
-					
-
-					///v2
-					///viewModel.scaleDownNot(results.index);
+					///expand and change color of the current marker
+					//self.activateCurrentMarker(results.index, "marker click");
+					///collapse and change color of the old marker
+					//self.changeOldMarker(results.index, "marker click");
 				});
 				///add to the 
 				self.nearbyMarkers.push(marker);
@@ -284,17 +279,65 @@ function Map()
 					viewModel.changeNearbyPlaces(self.nearbyPlaces);
 					self.nearbyPlaces.forEach(function(place){
 						$('#nearby-places-' + place.index).on('shown.bs.collapse', function(){
-							self.changeCurrentNearbyMarker(place.index);
-							console.log("shown");
+							//self.changeCurrentNearbyMarker(results.index);
+							///if the index is equal, either seeing a marker-opened event or have clicked on the same marker twice
+							///how to differentiate
+							if (self.currentNearbyPlace.index == place.index){
+								console.log("index equal")
+							} else 	if (self.currentNearbyPlace.index != place.index){
+								///this will be hit if user clicked on a new button
+								console.log("index not equal")
+
+								///expanded by itself
+								///change marker color
+								self.currentNearbyPlace = new self.NearbyPlaceObject(place.index);
+								console.log("current nearby place index " + self.currentNearbyPlace.index)
+								self.currentNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+							
+								///ensure there is an old place to modify
+								if (self.oldNearbyPlace != ""){
+									console.log("old nearby place exists and index is " + self.oldNearbyPlace.index);
+									///close old button, change old marker color
+									self.oldNearbyPlace.button.collapse("hide");
+									self.oldNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+									
+								}
+									//assign old button to be the new button
+									///asign old nearbyplace 
+									self.oldNearbyPlace = self.currentNearbyPlace;
+									console.log("old nearby place index is now " + self.oldNearbyPlace.index);
+							} else if (self.currentNearbyPlace.index == undefined){
+								///this will be hit if the first action is clicking a button
+								console.log("undefined")
+								///expanded by itself
+								///change marker color
+								self.currentNearbyPlace = self.NearbyPlaceObject(place.index);
+								self.currentNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+								///ensure there is an old place to modify
+								if (self.oldNearbyPlace != ""){
+									///close old button, change old marker color
+									self.oldNearbyPlace.button.collapse("hide");
+									self.oldNearbyPlace.marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+									
+									//assign old button to be the new button
+								}
+									//assign old button to be the new button
+									self.oldNearbyPlace = self.currentNearbyPlace;
+									console.log("old nearby place index is now " + self.oldNearbyPlace.index);
+							}
+							});
+					});
+							
+							
+
+							//self.openedByMarkerClick = false;
 
 							/*self.nearbyPlaces.forEach(function(otherPlace){
 								if (otherPlace.index != place.index){
-									console.log("collapse")
+									//console.log("collapse")
 									$('#nearby-places-' + otherPlace.index).collapse("hide");
 								};
 							});*/
-						});
-					});
 				}	
 			} else
 			{
@@ -316,8 +359,8 @@ function Map()
 
 	this.toggleNearbyCollapse = function(state){
 		if(self.currentNearbyPlace != ""){
+			//console.log("XXXXXXXXXXXXXXXXXX collapse nearby XXXXXXXXXXXXXXXXXXXXXX");
 			self.currentNearbyPlace.collapse(state);
-
 		}
 	}
 
